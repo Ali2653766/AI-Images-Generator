86798841bb3260573b19bb41adcf540ef26864ef
Ali Mahmmoud <127209252+Ali2653766@users.noreply.github.com>
2025-05-16 06:25:25 +0000

    Finished Project!

--- script.js
+++ script.js
@@ -16,6 +16,11 @@ function generateImage(){
 
     setLoadingState(true);
 
+
+    var myHeaders = new Headers();
+    myHeaders.append("Authorization", `Bearer ${api}` );
+    
+    
     const formData = new FormData();
     formData.append('prompt', prompt);
     formData.append('style', style);
@@ -28,13 +33,20 @@ function generateImage(){
         redirect: 'follow'
     };
 
-    var myHeaders = new Headers();
-    myHeaders.append("Authorization", `Bearer ${api}` );
-
+    
     fetch(url, requestOptions)
-        .then(response => response.text())
-        .then(result => console.log(result))
-        .catch(error => console.log('error', error));
+        .then(response => response.blob())
+        .then(blob =>{
+            const imageUrl = URL.createObjectURL(blob)
+            image.src = imageUrl
+        })
+        .catch(error => {
+            alert('An Error!')
+            console.log('error', error)
+        })
+        .finally(() => {
+            setLoadingState(false);
+        })
 }
 
 
@@ -46,5 +58,21 @@ function setLoadingState(isLoading){
         image.style.display = 'block';
         imageContainer.classList.remove('loading');
     }
+
+}
+
+function downloadImage(){
+    const imageUrl = image.src;
+
+    if(!imageUrl){
+        alert('The Url is Empty');
+        return;
+    }
+
+    const link = document.createElement('a');
+    link.href = imageUrl;
+    link.download = 'ai-generated-image.jpg'
+
+    link.click();
 
 }
